1148d037e1f7bca059005c6da23d409a81a43a38
Jacob Pfeifer <jakepf00@gmail.com>
2018-12-14 21:19:36 +0000

    Fixed piece movement

--- app/src/main/java/com/jakepf00/chess/RuleEngine.java
+++ app/src/main/java/com/jakepf00/chess/RuleEngine.java
@@ -21,33 +21,105 @@ class RuleEngine {
             case 'P': // fall through
             case 'p': // TODO: add en passant
                 if (targetSquare != ' ') {
-                    return ((Math.abs(y2 - y1) == 1) && (Math.abs(x2 - x1) == 1));
+                    return ((Math.abs(y2 - y1) == 1) && (x2 - x1 == -1));
                 }
                 else if (x1 == 6) {
-                    if (Math.abs(x2 - x1) == 2) {
-                        return ((y2 == y1) && (board[y1][x2 - 1] == ' '));
+                    if (x2 - x1 == -2) {
+                        return ((y2 == y1) && (board[x1 - 1][y1] == ' '));
                     } else {
-                        return ((y2 == y1) && (Math.abs(x2 - x1) == 1));
+                        return ((y2 == y1) && (x2 - x1 == -1));
                     }
                 }
                 else {
-                    return ((y2 == y1) && (Math.abs(x2 - x1) <= 1));
+                    return ((y2 == y1) && (x2 - x1) == -1);
                 }
             case 'K':
-            case 'k':
+            case 'k': // TODO: add castling
                 return (!(Math.abs(x2 - x1) > 1) && !(Math.abs(y2 - y1) > 1));
             case 'R':
-            case 'r': // TODO: make sure rooks don't jump pieces
-                return !((x2 != x1) && (y2 != y1));
+            case 'r':
+                if (x2 == x1) {
+                    for (int i = Math.min(y1, y2) + 1; i < Math.max(y1, y2); i++) {
+                        if (board[x1][i] != ' ') return false;
+                    }
+                    return true;
+                }
+                else if (y2 == y1) {
+                    for (int i = Math.min(x1, x2) + 1; i < Math.max(x1, x2); i++) {
+                        if (board[i][y1] != ' ') return false;
+                    }
+                    return true;
+                }
+                else return false;
             case 'N':
             case 'n':
                 return (((Math.abs(x2 - x1) == 2) && (Math.abs(y2 - y1) == 1)) || ((Math.abs(x2 - x1) == 1) && (Math.abs(y2 - y1) == 2)));
             case 'B':
-            case 'b': // TODO: make sure bishops don't jump pieces
-                return (Math.abs(y2 - y1) == Math.abs(x2 - x1));
+            case 'b':
+                if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
+                    if (x1 < x2 && y1 < y2) {
+                        for (int i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
+                            if (board[i][j] != ' ') return false;
+                        }
+                    }
+                    if (x1 < x2 && y1 > y2) {
+                        for (int i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
+                            if (board[i][j] != ' ') return false;
+                        }
+
+                    }
+                    if (x1 > x2 && y1 < y2) {
+                        for (int i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
+                            if (board[i][j] != ' ') return false;
+                        }
+                    }
+                    if (x1 > x2 && y1 > y2) {
+                        for (int i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
+                            if (board[i][j] != ' ') return false;
+                        }
+
+                    }
+                    return true;
+                }
+                else return false;
             case 'Q':
-            case 'q': // TODO: make sure queens don't jump pieces
-                return ((Math.abs(y2 - y1) == Math.abs(x2 - x1)) || !((x2 != x1) && (y2 != y1)));
+            case 'q':
+                if (x2 == x1) {
+                    for (int i = Math.min(y1, y2) + 1; i < Math.max(y1, y2); i++) {
+                        if (board[x1][i] != ' ') return false;
+                    }
+                }
+                else if (y2 == y1) {
+                    for (int i = Math.min(x1, x2) + 1; i < Math.max(x1, x2); i++) {
+                        if (board[i][y1] != ' ') return false;
+                    }
+                }
+                else if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
+                    if (x1 < x2 && y1 < y2) {
+                        for (int i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
+                            if (board[i][j] != ' ') return false;
+                        }
+                    }
+                    if (x1 < x2 && y1 > y2) {
+                        for (int i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
+                            if (board[i][j] != ' ') return false;
+                        }
+
+                    }
+                    if (x1 > x2 && y1 < y2) {
+                        for (int i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
+                            if (board[i][j] != ' ') return false;
+                        }
+                    }
+                    if (x1 > x2 && y1 > y2) {
+                        for (int i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
+                            if (board[i][j] != ' ') return false;
+                        }
+
+                    }
+                }
+                else return false;
+                return true;
             default:
                 return true;
         }
