a42aa4b9bc94fe6470d73cd215d3c6eeadc561a3
Jacob Pfeifer <jakepf00@gmail.com>
2018-12-17 19:43:13 +0000

    Added functions for the movement of all the pieces

--- app/src/main/java/com/jakepf00/chess/RuleEngine.java
+++ app/src/main/java/com/jakepf00/chess/RuleEngine.java
@@ -22,112 +22,7 @@ class RuleEngine {
         if (pieceToMove == ' ') return false; // Can't move a blank square
 
         // Piece specific checks
-        switch (pieceToMove) {
-            case 'P': // fall through
-            case 'p': // TODO: add en passant
-                if (targetSquare != ' ') {
-                    return ((Math.abs(y2 - y1) == 1) && (x2 - x1 == -1));
-                }
-                else if (x1 == 6) {
-                    if (x2 - x1 == -2) {
-                        return ((y2 == y1) && (board[x1 - 1][y1] == ' '));
-                    } else {
-                        return ((y2 == y1) && (x2 - x1 == -1));
-                    }
-                }
-                else {
-                    return ((y2 == y1) && (x2 - x1) == -1);
-                }
-            case 'K':
-            case 'k': // TODO: add castling
-                return (!(Math.abs(x2 - x1) > 1) && !(Math.abs(y2 - y1) > 1));
-            case 'R':
-            case 'r':
-                if (x2 == x1) {
-                    for (int i = Math.min(y1, y2) + 1; i < Math.max(y1, y2); i++) {
-                        if (board[x1][i] != ' ') return false;
-                    }
-                    return true;
-                }
-                else if (y2 == y1) {
-                    for (int i = Math.min(x1, x2) + 1; i < Math.max(x1, x2); i++) {
-                        if (board[i][y1] != ' ') return false;
-                    }
-                    return true;
-                }
-                else return false;
-            case 'N':
-            case 'n':
-                return (((Math.abs(x2 - x1) == 2) && (Math.abs(y2 - y1) == 1)) || ((Math.abs(x2 - x1) == 1) && (Math.abs(y2 - y1) == 2)));
-            case 'B':
-            case 'b':
-                if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
-                    if (x1 < x2 && y1 < y2) {
-                        for (int i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
-                            if (board[i][j] != ' ') return false;
-                        }
-                    }
-                    if (x1 < x2 && y1 > y2) {
-                        for (int i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
-                            if (board[i][j] != ' ') return false;
-                        }
-
-                    }
-                    if (x1 > x2 && y1 < y2) {
-                        for (int i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
-                            if (board[i][j] != ' ') return false;
-                        }
-                    }
-                    if (x1 > x2 && y1 > y2) {
-                        for (int i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
-                            if (board[i][j] != ' ') return false;
-                        }
-
-                    }
-                    return true;
-                }
-                else return false;
-            case 'Q':
-            case 'q':
-                if (x2 == x1) {
-                    for (int i = Math.min(y1, y2) + 1; i < Math.max(y1, y2); i++) {
-                        if (board[x1][i] != ' ') return false;
-                    }
-                }
-                else if (y2 == y1) {
-                    for (int i = Math.min(x1, x2) + 1; i < Math.max(x1, x2); i++) {
-                        if (board[i][y1] != ' ') return false;
-                    }
-                }
-                else if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
-                    if (x1 < x2 && y1 < y2) {
-                        for (int i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
-                            if (board[i][j] != ' ') return false;
-                        }
-                    }
-                    if (x1 < x2 && y1 > y2) {
-                        for (int i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
-                            if (board[i][j] != ' ') return false;
-                        }
-
-                    }
-                    if (x1 > x2 && y1 < y2) {
-                        for (int i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
-                            if (board[i][j] != ' ') return false;
-                        }
-                    }
-                    if (x1 > x2 && y1 > y2) {
-                        for (int i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
-                            if (board[i][j] != ' ') return false;
-                        }
-
-                    }
-                }
-                else return false;
-                return true;
-            default:
-                return true;
-        }
+        return pieceMove(board, move);
     }
 
     static char[][] flipBoard(char[][] board) {
@@ -154,5 +49,117 @@ class RuleEngine {
             }
         }
         return copy;
+    }
+
+    private static boolean pieceMove(char[][] board, Move move) {
+        switch (board[move.x1][move.y1]) {
+            case 'P':
+            case 'p':
+                return pawnMove(board, move);
+            case 'K':
+            case 'k':
+                return kingMove(move);
+            case 'R':
+            case 'r':
+                return rookMove(board, move);
+            case 'N':
+            case 'n':
+                return knightMove(move);
+            case 'B':
+            case 'b':
+                return bishopMove(board, move);
+            case 'Q':
+            case 'q':
+                return queenMove(board, move);
+            default:
+                return true;
+        }
+    }
+    private static boolean pawnMove(char[][] board, Move move) {
+        int x1 = move.x1;
+        int y1 = move.y1;
+        int x2 = move.x2;
+        int y2 = move.y2;
+        if (board[x2][y2] != ' ') {
+            return ((Math.abs(y2 - y1) == 1) && (x2 - x1 == -1));
+        }
+        else if (x1 == 6) {
+            if (x2 - x1 == -2) {
+                return ((y2 == y1) && (board[x1 - 1][y1] == ' '));
+            } else {
+                return ((y2 == y1) && (x2 - x1 == -1));
+            }
+        }
+        else {
+            return ((y2 == y1) && (x2 - x1) == -1);
+        }
+    }
+    private static boolean kingMove(Move move) {
+        int x1 = move.x1;
+        int y1 = move.y1;
+        int x2 = move.x2;
+        int y2 = move.y2;
+        return (!(Math.abs(x2 - x1) > 1) && !(Math.abs(y2 - y1) > 1));
+    }
+    private static boolean rookMove(char[][] board, Move move) {
+        int x1 = move.x1;
+        int y1 = move.y1;
+        int x2 = move.x2;
+        int y2 = move.y2;
+        if (x2 == x1) {
+            for (int i = Math.min(y1, y2) + 1; i < Math.max(y1, y2); i++) {
+                if (board[x1][i] != ' ') return false;
+            }
+            return true;
+        }
+        else if (y2 == y1) {
+            for (int i = Math.min(x1, x2) + 1; i < Math.max(x1, x2); i++) {
+                if (board[i][y1] != ' ') return false;
+            }
+            return true;
+        }
+        else return false;
+    }
+    private static boolean knightMove(Move move) {
+        int x1 = move.x1;
+        int y1 = move.y1;
+        int x2 = move.x2;
+        int y2 = move.y2;
+        return (((Math.abs(x2 - x1) == 2) && (Math.abs(y2 - y1) == 1)) || ((Math.abs(x2 - x1) == 1) && (Math.abs(y2 - y1) == 2)));
+    }
+    private static boolean bishopMove(char[][] board, Move move) {
+        int x1 = move.x1;
+        int y1 = move.y1;
+        int x2 = move.x2;
+        int y2 = move.y2;
+        if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
+            if (x1 < x2 && y1 < y2) {
+                for (int i = x1 + 1, j = y1 + 1; i < x2; i++, j++) {
+                    if (board[i][j] != ' ') return false;
+                }
+            }
+            if (x1 < x2 && y1 > y2) {
+                for (int i = x1 + 1, j = y1 - 1; i < x2; i++, j--) {
+                    if (board[i][j] != ' ') return false;
+                }
+
+            }
+            if (x1 > x2 && y1 < y2) {
+                for (int i = x1 - 1, j = y1 + 1; i > x2; i--, j++) {
+                    if (board[i][j] != ' ') return false;
+                }
+            }
+            if (x1 > x2 && y1 > y2) {
+                for (int i = x1 - 1, j = y1 - 1; i > x2; i--, j--) {
+                    if (board[i][j] != ' ') return false;
+                }
+
+            }
+            return true;
+        }
+        else return false;
+    }
+    private static boolean queenMove(char[][] board, Move move) {
+        return (rookMove(board, move) || bishopMove(board, move));
     }
 }
